df41573ff89eb20bc7b6e0b013b811e7267e7dd2
taiti09 <taitiabe1997@gmail.com>
2023-07-27 22:50:03 +0900

    metaタグ追加

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
 import ReactGA from "react-ga4";
+import { Helmet } from 'react-helmet';
 
 // Google Analytics 測定 ID を入力して設定
 ReactGA.initialize("G-FRQ21922ZE");
@@ -12,6 +13,9 @@ ReactGA.send("pageview");
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider>
+      <Helmet>
+        <title>山本彩曲名当てクイズ</title>
+      </Helmet>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
