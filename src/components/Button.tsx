1fcefcdb72c69977479c9d536312cbb211c393e3
taiti09 <taitiabe1997@gmail.com>
2023-08-06 23:24:02 +0900

    #6 Buttonコンポーネントのインポートが間違えていたため修正

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,16 @@
 import { Button } from "@chakra-ui/react"
-import { Start } from "../lib/quiz"
+import { useStart } from "../lib/quiz"
 
 type Props =  {
   isSpinning: boolean
 }
 export const QuizButton= (props:Props) => {
+  const { start } = useStart();
   const isSpinning = props.isSpinning;
   return (
     <Button
     bgGradient='linear(to-r, yellow.200, pink.400)'
-    onClick={() => Start()}
+    onClick={() => start()}
     disabled={isSpinning}
     borderRadius='3xl'
     _hover={{ bg: 'pink.400', cursor: 'pointer' }}
