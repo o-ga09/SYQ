b0572b95863543817932fe709391cc6ba028ecd2
taiti09 <taitiabe1997@gmail.com>
2023-08-07 23:57:48 +0900

    #6 軽微な修正

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,20 @@
 import { Button } from "@chakra-ui/react"
 import { useStart } from "../lib/quiz"
+import { useState } from "react";
 
-type Props =  {
-  isSpinning: boolean
-}
-export const QuizButton= (props:Props) => {
+export const QuizButton= () => {
   const { start } = useStart();
-  const isSpinning = props.isSpinning;
+  const [ isSpinning, setSpinning ] = useState(false);
+
+  const handleStart = () => {
+    setSpinning(!isSpinning);
+    start(isSpinning);
+  };
+
   return (
     <Button
     bgGradient='linear(to-r, yellow.200, pink.400)'
-    onClick={() => start()}
+    onClick={() => handleStart()}
     disabled={isSpinning}
     borderRadius='3xl'
     _hover={{ bg: 'pink.400', cursor: 'pointer' }}
