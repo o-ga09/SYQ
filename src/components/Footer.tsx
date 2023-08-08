69dded21bf2f83c4082c4eeeaed1484e02d5ae9c
taiti09 <taitiabe1997@gmail.com>
2023-08-09 00:08:34 +0900

    #1 ポリシーへのリンク作成

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,21 @@
-import { Box } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons';
+import { Box,Text,Link, Flex } from '@chakra-ui/react'
 
 
 export const Footer = () => {
   return (
     <Box bgGradient='linear(to-r, pink.500, yellow.200)' p={4} color="black" mt="auto">
-    copyright 2023 o-ga09
+      <Flex direction='row'>
+        <Text>
+          &copy; copyright 2023 o-ga09
+        </Text>
+        <Link href='https://chakra-ui.com' isExternal marginLeft={16}>
+          お問い合わせ／ご要望はこちら <ExternalLinkIcon mx='2px' />
+        </Link>
+        <Link href='https://chakra-ui.com' isExternal marginLeft={16}>
+          サイトポリシー <ExternalLinkIcon mx='2px' />
+        </Link>
+      </Flex>
     </Box>
   )
 };
