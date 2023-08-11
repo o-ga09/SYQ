2b900d3acff94beb9733a1aa633eec8cb0875343
taiti09 <taitiabe1997@gmail.com>
2023-08-11 10:36:03 +0900

    #3 早押しモード作成中

--- src/pages/QuickAnswer.tsx
+++ src/pages/QuickAnswer.tsx
@@ -1,56 +1,67 @@
 import { Box, Flex, Text } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react'
-import { answerList, questionList } from '../lib/const';
+import {  useState } from 'react'
+import { answerList, max, questionList } from '../lib/const';
 import { getUniqueRandomNumbers } from '../lib/util';
 import { SelectAnswer } from '../components/SelectAnswer';
 import { useAnswer } from '../lib/quiz';
+import { useTimer } from 'react-timer-hook';
+import { QuizButton } from '../components/Button';
 
 function QuickAnswer() {
-  const [countdown, setCountdown] = useState(5);
-  const [currentProblemIndex, setCurrentProblemIndex] = useState(0);
+  const expiryTimestamp = new Date();
+  expiryTimestamp.setSeconds(expiryTimestamp.getSeconds()+5);
   const { answer } = useAnswer();
+  const {seconds,start,restart } = useTimer({ autoStart:false, expiryTimestamp });
+  const [ isCorrect , setIsCorrect ] = useState(false);
+ 
+  const [ nazo1, setNazo1 ] = useState<string>('---');
+  const [ choice1, setChoice1 ] = useState<string>('---');
+  const [ choice2, setChoice2 ] = useState<string>('---');
+  const [ choice3, setChoice3 ] = useState<string>('---');
 
-  const [ choice1, setChoice1 ] = useState<string>(answerList[0]);
-  const [ choice2, setChoice2 ] = useState<string>(answerList[1]);
-  const [ choice3, setChoice3 ] = useState<string>(answerList[2]);
-
-  useEffect(() => {
-    const countdownInterval = setInterval(() => {
-      setCountdown((prevCountdown) => (prevCountdown > 1 ? prevCountdown - 1 : 5));
-    }, 1000);
-
-    const nextProblemTimeout = setTimeout(() => {
-      setCurrentProblemIndex(() => (Math.floor(Math.random() * questionList.length)));
-      // nazo1を除外した配列を作成
-      const index = questionList.indexOf(questionList[currentProblemIndex]);
-      const filteredArray: string[] = answerList.filter((item) => item !== answerList[index]);
-    
-      // ランダムに２つの要素を取得
-      const getRandomElements = (arr: string[], count: number): string[] => {
-        const shuffled = arr.sort(() => 0.5 - Math.random());
-        return shuffled.slice(0, count);
-      };
-    
-      const result: string[] = getRandomElements(filteredArray, 2);
-      result.push(answerList[index]);
-      const randomNumbers = getUniqueRandomNumbers(3);
-      setChoice1(result[randomNumbers[0]]);
-      setChoice2(result[randomNumbers[1]]);
-      setChoice3(result[randomNumbers[2]]);
-      setCountdown(5);
-    }, 5000);
-
-    return () => {
-      clearInterval(countdownInterval);
-      clearTimeout(nextProblemTimeout);
+  const setQuestion = () => {
+    const question = questionList[Math.floor(Math.random() * max)];
+    // nazo1を除外した配列を作成
+    const index = questionList.indexOf(question);
+    const filteredArray: string[] = answerList.filter((item) => item !== answerList[index]);
+  
+    // ランダムに２つの要素を取得
+    const getRandomElements = (arr: string[], count: number): string[] => {
+      const shuffled = arr.sort(() => 0.5 - Math.random());
+      return shuffled.slice(0, count);
     };
-  }, [currentProblemIndex]);
+  
+    const result: string[] = getRandomElements(filteredArray, 2);
+    result.push(answerList[index]);
+    const randomNumbers = getUniqueRandomNumbers(3);
+    setNazo1(question);
+    setChoice1(result[randomNumbers[0]]);
+    setChoice2(result[randomNumbers[1]]);
+    setChoice3(result[randomNumbers[2]]);
+  };
 
-  const currentProblem = questionList[currentProblemIndex];
+  const TimerStart = () => {
+    const expiryTimestamp = new Date();
+    expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + 5);
+    setQuestion();
+    start();
+  };
+
+  const TimerRestart = () => {
+    const time = new Date();
+    time.setSeconds(time.getSeconds() + 5);
+    setQuestion();
+    restart(time);
+  };
   const handleAnswer = (no:number) => {
-    const res = answer(no,currentProblem,choice1,choice2,choice3);
-    console.log(res);
+    const res = answer(no,nazo1,choice1,choice2,choice3);
+    if(res.indexOf('正解') === -1) {
+      setIsCorrect(false);
+    } else {
+      setIsCorrect(true);
+    }
+    TimerRestart();
   };
 
   return (
@@ -61,14 +72,18 @@ function QuickAnswer() {
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5 }}
           >
-            <Text fontSize="xl">次の問題まで: {countdown}秒</Text>
+            <Text fontSize="xl">次の問題まで: {seconds}秒</Text>
             <Text mt={4} fontSize="xl" fontWeight="bold">
-              {currentProblem}
+              {nazo1}
             </Text>
           </motion.div>
         </Box>
 
         <SelectAnswer answer1={choice1} answer2={choice2} answer3={choice3} onClick={handleAnswer} />
+
+        <Text>{isCorrect ? '正解' : '不正解'}</Text>
+
+        <QuizButton title='早押しスタート' start={TimerStart} />
       </Flex>
   );
 }
